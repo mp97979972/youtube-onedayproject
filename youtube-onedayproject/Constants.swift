1809affdf255415d524577acc54cf39f9c9bbe8b
Marvin Ploch <m.ploch@mannheim-medizinelektronik.de>
2024-01-06 16:17:21 +0100

    bugfix: typo in .gitignore

--- youtube-onedayproject/Constants.swift
+++ youtube-onedayproject/Constants.swift
@@ -6,11 +6,3 @@
 //
 
 import Foundation
-
-struct Constants {
-    
-    static var API_KEY = "AIzaSyAFhKWyzmbmvc3_Nhg8Re7PuHOisyKE3Gw"
-    static var PLAYLIST_ID = "UULFF_n906__yoMtxZQtmqmZbA"
-    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-    
-}
