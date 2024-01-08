e94696593a70d989dc3b02062b9dd964574cf4b3
Marvin Ploch <m.ploch@mannheim-medizinelektronik.de>
2024-01-08 07:43:50 +0100

    added and custom tableview

--- youtube-onedayproject/Model.swift
+++ youtube-onedayproject/Model.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    func videosFetched(_ videos: [Video])
+}
+
 class Model {
+    
+    var delegate: ModelDelegate?
     
     func getVideos() {
         
@@ -30,6 +36,15 @@ class Model {
                 decoder.dateDecodingStrategy = .iso8601
                 
                 let response = try decoder.decode(Response.self, from: data!)
+                
+                if response.items != nil {
+                    // Call the videosFetched method of the delegate
+                    DispatchQueue.main.async {
+                        self.delegate?.videosFetched(response.items!)
+                    }
+                    
+                }
+                
                 
                 dump(response)
             }
